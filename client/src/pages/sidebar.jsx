e039f84bf87dbd5523799136d911371f2cc4f4e1
SlangItUp <abhi@slangitup.xyz>
2024-06-19 20:32:24 +0545

    [bug] fix online user number before socket connects

--- client/src/pages/sidebar.jsx
+++ client/src/pages/sidebar.jsx
@@ -7,6 +7,7 @@ import { useSocketContext } from "../context/SocketContext.jsx";
 function SideBar() {
   const {loading, conversations} = useGetConversations()
   const { onlineUsers } = useSocketContext()
+  console.log(onlineUsers)
   return (
     <div className="flex flex-col  pl-6 pr-2 w-64 bg-white flex-shrink-0">
       <ProfileCard />
@@ -14,11 +15,10 @@ function SideBar() {
         <div className="flex flex-row items-center justify-between text-xs">
           <span className="text-lg font-bold">Online Users</span>
           <span className="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full">
-            {onlineUsers.length - 1}
+            {onlineUsers!=0 ? onlineUsers.length - 1 : 0}
           </span>
         </div>
         <div className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
-
           {conversations.map((conv)=>(
                <SideBarUser 
                key={conv._id}
